f415ffa643e3bd5eaacb8d8d623fbe93aa0f1f46
RobinDeWalvis <voetterrb1a@gmail.com>
2015-09-24 17:24:56 +0200

    Sole java conversion

--- src/microcat/converter/ColorConverter.java
+++ src/microcat/converter/ColorConverter.java
@@ -1,161 +1,102 @@
 package microcat.converter;
 
+import java.awt.FlowLayout;
 import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+import java.awt.image.DataBufferInt;
 import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
-
-import org.lwjgl.opengl.ARBFragmentShader;
-import org.lwjgl.opengl.ARBVertexShader;
-import org.lwjgl.opengl.GL11;
-import org.lwjgl.opengl.GL13;
-
-import net.quantuminfinity.engine.gl.fbo.PingPongFBO;
-import net.quantuminfinity.engine.gl.shader.ShaderProgram;
-import net.quantuminfinity.engine.gl.util.GLMU;
-import net.quantuminfinity.engine.io.GLWindow;
-import net.quantuminfinity.engine.math.vector.Vector2;
-import net.quantuminfinity.engine.texture.TextureLoader;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class ColorConverter
-{	
-	static int SAMPLER_ID = 0, IMG_ID = 1;
-	
-	BufferedImage pal, img;
-	int imgWidth, imgHeight, imgTex, palTex;
-	String ofile;
-	
-	GLWindow w;
-	PingPongFBO fbo;
-	ShaderProgram conv, outp;
+{
+	int[] pal, src, dst;
+	int width, height;
 	
 	public ColorConverter(String ifile, String ofile) throws IOException
 	{
-		pal = ImageIO.read(ColorConverter.class.getResource("/assets/palette.png"));
+		BufferedImage palImg = ImageIO.read(ColorConverter.class.getResource("/assets/palette.png"));
+		BufferedImage srcImg = ImageIO.read(new File(ifile));
 		
-		img = ImageIO.read(new File(ifile));
-		imgWidth = img.getWidth();
-		imgHeight = img.getHeight();
+		width = srcImg.getWidth();
+		height = srcImg.getHeight();
 		
-		this.ofile = ofile;
+		pal = toPixelArray(palImg);
+		src = toPixelArray(srcImg);
+		dst = new int[src.length]; // the alpha component of the dst image is used to store the color's index on the palette.
 	}
 	
 	public void genImage()
 	{
-		init();
-		work();
-		close();
+		long startTime = System.nanoTime();
+		
+		for (int i=0; i<src.length; i++)
+			dst[i] = getClosestColor(src[i]);
+		
+		long endTime = System.nanoTime();
+		long cTime = (endTime - startTime) / 1000000;
+		
+		System.out.println("Conversion took " + cTime + " ms");
+		
+		show(toImage(dst, width, height));
 	}
+	
+	private void show(BufferedImage img)
+	{
+		JFrame frame = new JFrame();
+		frame.getContentPane().setLayout(new FlowLayout());
+		frame.getContentPane().add(new JLabel(new ImageIcon(img)));
+		frame.pack();
+		frame.setVisible(true);
+	}
+	
+	private int getClosestColor(int color)
+	{
+		int closestDst = Integer.MAX_VALUE, col = 0, dst;
 
-	private void init()
-	{
-		w = new GLWindow(imgWidth, imgHeight, "", false);
-		GLMU.initGL2D(w.getWidth(), w.getHeight());
-		GL11.glEnable(GL11.GL_TEXTURE_2D);
-		GL11.glClearColor(0, 0, 0, 1);
-		
-		fbo = new PingPongFBO(1, new Vector2(w.getWidth(), w.getHeight()), GL11.GL_RGBA8);
-		fbo.setData(TextureGenerator.genTex16(w.getWidth(), w.getHeight()));
-		
-		System.out.println("Creating conversion shader...");
-		conv = new ShaderProgram()
-		{{
-			addShader("pkg:/shaders/shader.vert", ARBVertexShader.GL_VERTEX_SHADER_ARB);
-			addShader("pkg:/shaders/conv.frag", ARBFragmentShader.GL_FRAGMENT_SHADER_ARB);
-			compile();
-		}};
-		
-		System.out.println("Creating output shader...");
-		outp = new ShaderProgram()
-		{{
-			addShader("pkg:/shaders/shader.vert", ARBVertexShader.GL_VERTEX_SHADER_ARB);
-			addShader("pkg:/shaders/outp.frag", ARBFragmentShader.GL_FRAGMENT_SHADER_ARB);
-			compile();
-		}};
-		
-		conv.bind();
-		conv.setUniform("uData", SAMPLER_ID);
-		conv.setUniform("uImg", IMG_ID);
-		conv.setUniform("uResolution", w.getWidth(), w.getHeight());
-		conv.release();
-		
-		outp.bind();
-		outp.setUniform("uData", SAMPLER_ID);
-		outp.setUniform("uPal", IMG_ID);
-		outp.setUniform("uResolution", w.getWidth(), w.getHeight());
-		outp.release();
-		
-		imgTex = TextureLoader.loadTexture(img, GL11.GL_NEAREST, GL11.GL_RGBA8);
-		palTex = TextureLoader.loadTexture(pal, GL11.GL_NEAREST, GL11.GL_RGBA8);
-	}
-	
-	private void work()
-	{
-		for (int i=0; i<pal.getWidth()*pal.getHeight(); i++)
-		{
-			int rgb = pal.getRGB(i%pal.getWidth(), i/pal.getWidth());
-			float r = (rgb >> 16 & 0xFF) / 256f;
-			float g = (rgb >>  8 & 0xFF) / 256f;
-			float b = (rgb >>  0 & 0xFF) / 256f;
-			
-			fbo.bind();
-			GL13.glActiveTexture(GL13.GL_TEXTURE0 + SAMPLER_ID); GL11.glBindTexture(GL11.GL_TEXTURE_2D, fbo.getData(0));
-			GL13.glActiveTexture(GL13.GL_TEXTURE0 + IMG_ID); GL11.glBindTexture(GL11.GL_TEXTURE_2D, imgTex);
-			conv.bind();
-			conv.setUniform("uColor", r, g, b);
-			conv.setUniform("uColorID", i/256f);
-			conv.setUniform("uColorUV", (float)(i%pal.getWidth())/pal.getWidth(), (float)(i/pal.getWidth())/pal.getHeight());
-			
-			GL11.glBegin(GL11.GL_QUADS);
+		for (int i=0; i<pal.length; i++)
+			if ((dst = ColorUtil.getDifferenceSq(color, pal[i])) < closestDst)
 			{
-				GL11.glTexCoord2f(0.0f, 0.0f); GL11.glVertex2f(0, 0);
-				GL11.glTexCoord2f(1.0f, 0.0f); GL11.glVertex2f(w.getWidth(), 0);
-				GL11.glTexCoord2f(1.0f, 1.0f); GL11.glVertex2f(w.getWidth(), w.getHeight());
-				GL11.glTexCoord2f(0.0f, 1.0f); GL11.glVertex2f(0, w.getHeight());
+				closestDst = dst;
+				col = i;
 			}
-			GL11.glEnd();
-			
-			conv.release();
-			fbo.release();
-			w.update();
-		}
 		
-		while (!w.isCloseRequested())
-		{
-			GL11.glClear(GL11.GL_COLOR_BUFFER_BIT);
-			GL11.glLoadIdentity();
-			
-			GL13.glActiveTexture(GL13.GL_TEXTURE0); GL11.glBindTexture(GL11.GL_TEXTURE_2D, fbo.getData(0));
-			GL13.glActiveTexture(GL13.GL_TEXTURE0 + IMG_ID); GL11.glBindTexture(GL11.GL_TEXTURE_2D, palTex);
-			outp.bind();
-			GL11.glBegin(GL11.GL_QUADS);
-				GL11.glVertex2f(0, 0);
-				GL11.glVertex2f(w.getWidth(), 0);
-				GL11.glVertex2f(w.getWidth(), w.getHeight());
-				GL11.glVertex2f(0, w.getHeight());
-			GL11.glEnd();
-			outp.release();
-			
-			w.update();
-		}
+		return ColorUtil.setAlpha(pal[col], col);
 	}
 	
-	private void close()
+	private int[] toPixelArray(BufferedImage img)
 	{
-		GL11.glDeleteTextures(imgTex);
-		GL11.glDeleteTextures(palTex);
-		outp.destroy();
-		conv.destroy();
-		fbo.destroy();
-		w.close();
-	}
+		byte[] pixels = ((DataBufferByte) img.getRaster().getDataBuffer()).getData();
+		int[] data = new int[img.getWidth() * img.getHeight()];
+		boolean hasAlpha = img.getAlphaRaster() != null;
 		
+		if (hasAlpha)
+			for (int i=0; i<data.length; i++)
+				data[i] = ColorUtil.asColor(pixels[i*4+3] & 0xFF, pixels[i*4+2] & 0xFF, pixels[i*4+1] & 0xFF, 0);
+		else
+			for (int i=0; i<data.length; i++)
+				data[i] = ColorUtil.asColor(pixels[i*3+2] & 0xFF, pixels[i*3+1] & 0xFF, pixels[i*3] & 0xFF, 0);
+		
+		return data;
+	}
+	
+	private BufferedImage toImage(int[] pixels, int width, int height)
+	{
+		BufferedImage img = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+		int[] data = ((DataBufferInt) img.getRaster().getDataBuffer()).getData();
+		System.arraycopy(pixels, 0, data, 0, pixels.length);
+		return img;
+	}
+	
 	public static void main(String[] args)
 	{
 		try
 		{
-			new ColorConverter("earth.jpg","").genImage();
+			new ColorConverter("spectrum.png","").genImage();
 		} catch (IOException e)
 		{
 			e.printStackTrace();
---
+++ src/microcat/converter/ColorUtil.java
@@ -0,0 +1,72 @@
+package microcat.converter;
+
+public class ColorUtil
+{
+	public static final int BIT_ALPHA = 24;
+	public static final int BIT_RED = 16;
+	public static final int BIT_GREEN = 8;
+	public static final int BIT_BLUE = 0;
+	
+	public static int asColor(int r, int g, int b, int a)
+	{
+		return  (clampColor(a) << BIT_ALPHA) | (clampColor(r) << BIT_RED) | (clampColor(g) << BIT_GREEN) | (clampColor(b) << BIT_BLUE);
+	}
+	
+	public static int getAlpha(int c)
+	{
+		return getColorByte(c, BIT_ALPHA);
+	}
+	
+	public static int getRed(int c)
+	{
+		return getColorByte(c, BIT_RED);
+	}
+	
+	public static int getGreen(int c)
+	{
+		return getColorByte(c, BIT_GREEN);
+	}
+	
+	public static int getBlue(int c)
+	{
+		return getColorByte(c, BIT_BLUE);
+	}
+	
+	public static int setAlpha(int c, int a)
+	{
+		return c | (clampColor(a) << BIT_ALPHA);
+	}
+	
+	public static int setRed(int c, int a)
+	{
+		return c | (clampColor(a) << BIT_RED);
+	}
+	
+	public static int setGreen(int c, int a)
+	{
+		return c | (clampColor(a) << BIT_GREEN);
+	}
+	
+	public static int setBlue(int c, int a)
+	{
+		return c | (clampColor(a) << BIT_BLUE);
+	}
+	
+	public static int getDifferenceSq(int c0, int c1)
+	{
+		int r = getRed(c0) - getRed(c1);
+		int g = getGreen(c0) - getGreen(c1);
+		int b = getBlue(c0) - getBlue(c1);
+		return r*r + g*g + b*b;
+	}
+	
+	private static int getColorByte(int c, int bit)
+	{
+		return c >> bit & 0xFF;
+	}
+	
+	private static int clampColor(int c)
+	{
+		return c > 0xFF ? 0xFF : c < 0 ? 0 : c;
+	}
+}
